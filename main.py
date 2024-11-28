5371020e50ac6306713e6bc376a0b78617705e6c
andrea <adipasquale@studenti.apuliadigitalmaker.it>
2024-11-28 11:06:21 +0100

    corretto

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from handlers.handler import router
 from handlers.impianti_handler import router_impianti
 from handlers.macchinari_handler import router_macchinari
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
 
@@ -10,6 +11,15 @@ app = FastAPI(
     docs_url='/documentation/',
 
     redoc_url=None
+)
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
 )
 
 app.include_router(router)
